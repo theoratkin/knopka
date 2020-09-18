de24ca0280398ef8235de05bf7c95dd81cb26ad1
nheelyolis <feogenratkin *at* gmail.com>
2020-09-18 11:23:28 -0400

    Write my own first-person controller that uses Character Collider and the new Input System

---
+++ Assets/script/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Transform Position;
+
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!enabled)
+            return;
+        Player player = collider.transform.GetComponent<Player>();
+        if (player && player.Checkpoint != Position) {
+            player.Checkpoint = Position;
+            Debug.LogFormat("{0}: checkpoint set", transform.parent.name);
+        }
+    }
+
+    void Update()
+    {
+    }
+}
